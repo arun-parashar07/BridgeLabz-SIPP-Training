b9df09d98e34348a2925d17cb66fc7ce90939a3c
Arun Parashar <134221620+arun-parashar07@users.noreply.github.com>
2025-06-17 09:39:36 +0530

    Delete Day2 directory

--- Day2/ques3.java
+++
@@ -1,8 +0,0 @@
-//Create a program to convert the distance of 10.8 kilometers to miles.
-public class ques3 {
-	public static void main(String[] args) {
-		double dist_inkms = 10.8;
-		double miles = dist_inkms * 0.621371 ; 
-		System.out.println(miles);
-	}
-}
